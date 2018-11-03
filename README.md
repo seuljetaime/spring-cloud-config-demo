f120b4b4d1461c07d0972f56e7376d98afebd528
zhuhongyu <zhuhongyu@gzkit.com.cn>
2018-11-03 15:24:20 +0800

    完成config client的代码

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # spring-cloud-config-demo
+
+
+
+# Config Server
+
+## 初始化git
+
+where `${user.home}/config-repo` is a git repository containing YAML and properties files.
+
+On Windows, you need an extra "/" in the file URL if it is absolute with a drive prefix (for example,`file:///${user.home}/config-repo`).
+
+```
+$ cd $HOME
+$ mkdir config-repo
+$ cd config-repo
+$ git init .
+$ echo server.port: 8080 > application.properties
+$ echo info.foo: bar >> application.properties
+$ git add -A .
+$ git commit -m "Add application.properties"
+```
+
+## 启动并访问
+
+没有加入spring security，可以直接访问
+
+http://localhost:8888/application/testing
+
+http://localhost:8888/testing/application.yml
+
+
+
+**TODO: testing 可以随意输入，用abcd也能看到值**
+
+
+
+# Config Client
+
+启动并访问 http://localhost:8080/test
--- config-client/src/main/java/client/ConfigClientApplication.java
+++ config-client/src/main/java/client/ConfigClientApplication.java
@@ -1,15 +1,17 @@
 package client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 @RestController("/")
 public class ConfigClientApplication {
+
+    @Value("${info.foo}")
+    private String property;
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
@@ -17,6 +19,6 @@ public class ConfigClientApplication {
 
     @GetMapping("/test")
     public String test() {
-        return "This is a test";
+        return "This is a test, the property is: " + property;
     }
 }
